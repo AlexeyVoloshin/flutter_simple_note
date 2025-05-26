22117c0e8a96b20726d9558075048c7bd5d16ce1
Voloshin Olexii <voloshinalex1100@gmail.com>
2025-05-26 18:26:40 +0300

    fixed animation cart note

--- lib/widgets/card_note.dart
+++ lib/widgets/card_note.dart
@@ -5,9 +5,7 @@ import 'package:hello_flutter/models/note.dart';
 
 class CardNote extends StatelessWidget {
   final Note note;
-
   final dynamic onLongPress;
-
   final dynamic onTap;
 
   const CardNote({
@@ -20,36 +18,37 @@ class CardNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ColorBloc, int>(
-      builder: (context, currentColor) => AnimatedContainer(
+      builder: (context, _) => AnimatedContainer(
         duration: Duration(milliseconds: 500),
-        decoration: BoxDecoration(
-          color: Color(note.colorValue), 
-          borderRadius: BorderRadius.circular(16), 
-        ),
-        child: InkWell(
-          onTap: onTap,
-          onLongPress: onLongPress,
-          child: Padding(
-            padding: const EdgeInsets.all(17),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  note.title,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.black,
+        child: Material(
+          borderRadius: BorderRadius.circular(16),
+          color: Color(note.colorValue),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(16),
+            onTap: onTap,
+            onLongPress: onLongPress,
+            child: Padding(
+              padding: const EdgeInsets.all(17),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    note.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  note.description,
-                  maxLines: 10,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontSize: 14, color: Colors.black87),
-                ),
-              ],
+                  const SizedBox(height: 8),
+                  Text(
+                    note.description,
+                    maxLines: 10,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontSize: 14, color: Colors.black87),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
